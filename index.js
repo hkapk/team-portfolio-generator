853d455f7aa90ed6277aa9bff4e06cfab3d8a49e
Hank Kapka <hankkapka@gmail.com>
2021-11-26 09:56:46 -0600

    completed initial question prompts for manager

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const inquirer = require('inquirer');
 
-const questions = [{
-    type: 'input',
-    name: 'manager',
-    message: 'Enter the name of the Manager.',
-}];
+const questions = [
+        {
+        type: 'input',
+        name: 'manager',
+        message: 'Enter the name of the manager.',
+        },
+        {
+        type: 'input',
+        name: 'id',
+        message: 'Enter the employee ID of the manager.',
+        },
+        {
+        type: 'input',
+        name: 'email',
+        message: 'Enter the email address of the manager.',
+        },
+        {
+        type: 'input',
+        name: 'office',
+        message: 'Enter the office number for the manager',
+        },
+
+];
 
 function init () {
     inquirer.prompt(questions);

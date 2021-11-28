417e53053c5b698bc35a6346e8547cc4bc584d4c
Hank Kapka <hankkapka@gmail.com>
2021-11-27 18:47:11 -0600

    updating classes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require('inquirer');
 const generateTeam = require('./src/page-template');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
 const fs = require('fs');
 
 
